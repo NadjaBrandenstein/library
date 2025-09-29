90d4f506228a138014e4dd4016623aa7423c577c
NadjaBrandenstein <nadbra01@easv365.dk>
2025-09-29 19:05:15 +0200

    gitignore

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,40 @@
+using api;
+using efscaffold;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
+// builder.Services.AddScoped<ILibraryService<BookDto, CreateBookDto, UpdateBookDto>, BookService>();
+// builder.Services.AddScoped<ILibraryService<AuthorDto, CreateAuthorDto, UpdateAuthorDto>, AuthorService>();
+// builder.Services.AddScoped<ILibraryService<GenreDto, CreateGenreDto, UpdateGenreDto>, GenreService>();
+// builder.Services.AddScoped<ILibraryService<BookImageDto, CreateBookImageDto, UpdateBookImageDto>, BookImageService>();
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql(appOptions.DbConnectionString);
+});
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
+builder.Services.AddProblemDetails();
+//builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandler();
+
+app.UseCors(config => config
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin()
+    .SetIsOriginAllowed(x => true));
+
+app.MapControllers();
+app.UseOpenApi();
+app.UseSwaggerUi();
+//await app.GenerateApiClientsFromOpenApi("/../../client/src/generated-ts-client.ts");
 
 app.Run();
