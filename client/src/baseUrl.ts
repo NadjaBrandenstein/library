cd46b655f18fb6fc060c7bc69ed5d01a3a9ce365
NadjaBrandenstein <nadbra01@easv365.dk>
2025-09-29 23:17:47 +0200

    Deployment of client

---
+++ client/src/baseUrl.ts
@@ -0,0 +1,10 @@
+﻿
+const isProduction = import.meta.env.prod;
+
+const baseUrl = isProduction
+    ? "https://library-project-api.fly.dev"
+    : "http://localhost:5063";
+
+// export const bookClient = new BookClient(baseUrl);
+// export const authorClient = new AuthorClient(baseUrl);
+// export const bookImageClient = new BookImageClient(baseUrl);
